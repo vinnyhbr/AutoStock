943935a72c63f35b5bf611a84d8c1054fc405be4
Vinícius Rosa <viniciusrosalima14@gmail.com>
2025-09-15 20:47:43 -0300

    first commit

---
+++ AutoStock/src/main/java/com/vini/autostock/AutoStockApplication.java
@@ -0,0 +1,13 @@
+package com.vini.autostock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoStockApplication.class, args);
+    }
+
+}
