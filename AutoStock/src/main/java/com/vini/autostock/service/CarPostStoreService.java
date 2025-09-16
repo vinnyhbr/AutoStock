4eb76816a6be3e1418d35bec696c3b691c250842
Vinícius Rosa <viniciusrosalima14@gmail.com>
2025-09-15 21:35:59 -0300

    feat: creating the carpost services interface

---
+++ AutoStock/src/main/java/com/vini/autostock/service/CarPostStoreService.java
@@ -0,0 +1,18 @@
+package com.vini.autostock.service;
+
+import com.vini.autostock.dto.CarPostDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface CarPostStoreService {
+
+    List<CarPostDTO> getCarForSales();
+
+    void changeCarForSale(CarPostDTO carPostDTO, String id);
+
+    void removeCarForSale(String id);
+
+
+}
