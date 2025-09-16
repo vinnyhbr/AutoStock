1b5b87283e47db2630abb9dfa7960c66d6507343
Vinícius Rosa <viniciusrosalima14@gmail.com>
2025-09-15 21:56:57 -0300

    feat: creating the method carForSalesClient && updating the variable name

--- AutoStock/src/main/java/com/vini/autostock/client/CarPostStoreClient.java
+++ AutoStock/src/main/java/com/vini/autostock/client/CarPostStoreClient.java
@@ -1,11 +1,24 @@
 package com.vini.autostock.client;
 
+import com.vini.autostock.dto.CarPostDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 @Component
 public class CarPostStoreClient {
 
     private final String USER_STORE_SERVICE_URI = "http://localhost:8080/user";
-    private final String CAR_STORE_SERVICE_URI = "http://localhost:8080/sales";
+    private final String POSTS_STORE_SERVICE_URI = "http://localhost:8080/sales";
 
+    RestTemplate restTemplate;
+
+    public List<CarPostDTO> carForSalesClient() {
+        ResponseEntity<CarPostDTO[]> responseEntity = restTemplate.getForEntity(POSTS_STORE_SERVICE_URI+"/cars",CarPostDTO[].class);
+        return Arrays.asList(Objects.requireNonNull(responseEntity.getBody()));
+    }
 }
