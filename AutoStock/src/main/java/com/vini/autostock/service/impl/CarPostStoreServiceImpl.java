0160e22c13edd0a051c30cc0e9b430f1212eb0ce
Vinícius Rosa <viniciusrosalima14@gmail.com>
2025-09-17 11:50:09 -0300

    a

--- AutoStock/src/main/java/com/vini/autostock/service/impl/CarPostStoreServiceImpl.java
+++ AutoStock/src/main/java/com/vini/autostock/service/impl/CarPostStoreServiceImpl.java
@@ -6,8 +6,8 @@ import com.vini.autostock.service.CarPostStoreService;
 import java.util.List;
 
 public class CarPostStoreServiceImpl implements CarPostStoreService {
-
-
+//só pra ficar vedrin
+//só pra ficar vedrin
     @Override
     public List<CarPostDTO> getCarForSales() {
         return List.of();
@@ -23,3 +23,7 @@ public class CarPostStoreServiceImpl implements CarPostStoreService {
 
     }
 }
+//só pra ficar vedrin
+//só pra ficar vedrin
+//só pra ficar vedrin
+//só pra ficar vedrin
