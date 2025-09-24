8f64eccdfbb38a5a6ecbeea45a88066ddda58a5d
Vinícius Rosa <viniciusrosalima14@gmail.com>
2025-09-24 14:55:33 -0300

    feat: criando o resto dos metodos e injetando a dependencia do carPostStoreClient

--- AutoStock/src/main/java/com/vini/autostock/service/impl/CarPostStoreServiceImpl.java
+++ AutoStock/src/main/java/com/vini/autostock/service/impl/CarPostStoreServiceImpl.java
@@ -1,24 +1,31 @@
 package com.vini.autostock.service.impl;
 
+import com.vini.autostock.client.CarPostStoreClient;
 import com.vini.autostock.dto.CarPostDTO;
 import com.vini.autostock.service.CarPostStoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CarPostStoreServiceImpl implements CarPostStoreService {
+
+    @Autowired
+    private CarPostStoreClient carPostStoreClient;
 
     @Override
     public List<CarPostDTO> getCarForSales() {
-        return List.of();
+        return carPostStoreClient.carForSalesClient();
     }
 
     @Override
-    public void changeCarForSale(CarPostDTO carPostDTO, String id) {
-
+    public void changeCarForSale(CarPostDTO carPost, String id) {
+        carPostStoreClient.changeCarForSaleClient(carPost, id);
     }
 
     @Override
     public void removeCarForSale(String id) {
-
+        carPostStoreClient.deleteCarForSaleClient(id);
     }
 }
